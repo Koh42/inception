8228b9976db3fd069271237a2c6e42ffd18125d1
Koh <ec2-user@ip-172-31-41-37.ap-southeast-1.compute.internal>
2023-03-29 07:53:54 +0800

    fix ftp conf and lesser port

--- srcs/requirements/bonus/ftp/tools/entrypoint.sh
+++ srcs/requirements/bonus/ftp/tools/entrypoint.sh
@@ -4,6 +4,6 @@ adduser -h /wordpress -s /sbin/nologin -u $FTP_UID -D -H $FTP_USER
 printf "${FTP_PASS}\n${FTP_PASS}" | passwd $FTP_USER
 
 IP=`wget -qO- https://api.ipify.org`
-echo pasv_address=$IP >> /etc/vsftpd/vsftpd.conf
+sed -i "s/.*pasv_address=.*/pasv_address=$IP/" /etc/vsftpd/vsftpd.conf
 
 exec "$@"
