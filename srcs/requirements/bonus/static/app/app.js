3dc99efd6f2fd54a4fbfb06f988afbf64b1c0204
Koh <ec2-user@ip-172-31-41-37.ap-southeast-1.compute.internal>
2023-03-29 08:06:40 +0800

    fix static port

--- srcs/requirements/bonus/static/app/app.js
+++ srcs/requirements/bonus/static/app/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 80
+const port = process.argv[2] || 8000
 app.use(express.static('public'))
 app.listen(port, () => {
   console.log(`Expressjs app listening on port ${port}`)
