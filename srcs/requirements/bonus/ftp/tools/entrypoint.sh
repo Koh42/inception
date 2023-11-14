ab1423446d53db1a53b7308070ba0d6567b20534
Koh42 <83402906+Koh42@users.noreply.github.com>
2023-11-15 01:39:53 +0800

    fix echo

--- srcs/requirements/bonus/ftp/tools/entrypoint.sh
+++ srcs/requirements/bonus/ftp/tools/entrypoint.sh
@@ -6,9 +6,9 @@ NOBODY=`id -u nobody`
 sed -i "s/^\(${FTP_USER}:x:\)[^\:]\+\(.*\)/\1${NOBODY}\2/" /etc/passwd
 
 #https://api.ipify.org crash!?
-@echo Querying public IP for pasv_addr
+echo Querying public IP for pasv_addr
 IP=`wget -qO- https://api.my-ip.io/ip`
 sed -i "s/.*pasv_address=.*/pasv_address=$IP/" /etc/vsftpd/vsftpd.conf
 
-@echo vsftpd starts
+echo vsftpd starts
 exec "$@"
